4e4b9edc5e512fb2114933b8be3f7b063acbebc1
Veritaserum04 <amruthabhatv@gmail.com>
2026-08-01 04:03:23 +0530

    Configure Gradio app for Render deployment

--- app.py
+++ app.py
@@ -41,4 +41,9 @@ demo = gr.Interface(
     description="Upload a leaf image to detect plant diseases."
 )
 
-demo.launch()
+import os
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
